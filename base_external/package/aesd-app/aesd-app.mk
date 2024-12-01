b4e9f7bd93f17d07d524f54c47b30e5024f19f0f
Trapti Balgi <trba9886@colorado.edu>
2024-11-30 21:48:30 -0700

    AESD_APP updates

---
+++ base_external/package/aesd-app/aesd-app.mk
@@ -0,0 +1,25 @@
+
+##############################################################
+#
+# AESD-APP
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_APP_VERSION = '32a9be5954e85845a56c2677c7edd4f034909aa6'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_APP_SITE = 'git@github.com:chwe3468/cu-ecen-5013-final-project-shared.git'
+AESD_APP_SITE_METHOD = git
+
+define AESD_APP_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/Chutao/aesd-cam all
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define AESD_APP_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/Chutao/aesd-cam/capture $(TARGET_DIR)/home
+endef
+
+$(eval $(generic-package))
